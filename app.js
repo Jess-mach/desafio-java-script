5b6c90fb16da885fb8baa8249f44ff4bdae26f91
Jess-mach <machadojessica1997@gmail.com>
2025-01-17 23:59:29 -0300

    desafio

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector ('h1');
+titulo.innerHTML = 'Jogo do Numero Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número de 1 a 100.';
